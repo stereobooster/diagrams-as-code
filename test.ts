799ca4a38d82325f2ae9b464446ce20f4bdc922a
stereobooster <stereobooster@gmail.com>
2025-04-25 01:26:27 +0200

    ci

--- test.ts
+++
@@ -1,5 +0,0 @@
-import { readFileSync } from "node:fs";
-import { render } from "./src/index.js";
-
-const filePath = "./examples/message-collecting-gcp.yaml";
-console.log(render(readFileSync(filePath, "utf8")));
