d2cefe8104004cd9b38d247210dc1170e432f6df
stereobooster <stereobooster@gmail.com>
2025-04-25 19:53:53 +0200

    screenshots

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,9 @@
 import fs from "node:fs/promises";
 import { describe, expect, it } from "vitest";
+import { Resvg } from "@resvg/resvg-js";
 
 import { render } from "../src";
+import { writeFile } from "node:fs/promises";
 
 describe("render", () => {
   const files = [
@@ -14,7 +16,7 @@ describe("render", () => {
     "workers-aws",
   ];
   files.forEach((name) => {
-    it(name, async () => {
+    it(`${name}.svg`, async () => {
       const file = await fs.readFile(
         new URL(`./yaml/${name}.yaml`, import.meta.url)
       );
@@ -22,6 +24,35 @@ describe("render", () => {
       await expect(render(file.toString())).toMatchFileSnapshot(
         `./svg/${name}.svg`
       );
+    });
+
+    // skipped because very slow
+    it.skip(`${name}.png`, async () => {
+      const file = await fs.readFile(
+        new URL(`./yaml/${name}.yaml`, import.meta.url)
+      );
+
+      const resvg = new Resvg(render(file.toString()));
+      const resolved = await Promise.all(
+        resvg.imagesToResolve().map(async (url) => {
+          const img = await fetch(url);
+          const buffer = await img.arrayBuffer();
+          return {
+            url,
+            buffer: Buffer.from(buffer),
+          };
+        })
+      );
+      if (resolved.length > 0) {
+        for (const result of resolved) {
+          const { url, buffer } = result;
+          resvg.resolveImage(url, buffer);
+        }
+      }
+      const png = resvg.render().asPng();
+      await writeFile(`./test/png/${name}.png`, png);
+
+      // await expect(png).toMatchFileSnapshot(`./png-snapshot/${name}.snapshot`);
     });
   });
 });
